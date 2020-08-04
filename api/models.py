f1949c34a48eaf1e363e9541e55e370d37db9c52
Shubhashish Verma <shubhashish.verma@f1studioz.com>
2020-08-04 10:20:27 +0530

    Initial Commit

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User, Group, AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+# class User(AbstractUser):
+#     is_student = models.BooleanField('student status', default=False)
+#     is_teacher = models.BooleanField('teacher status', default=False)
+
+# class Feedback(models.Model):
+
+# class Meeting(models.Model):
