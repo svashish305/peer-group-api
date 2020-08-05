978d291b698da379fa1439c3abeef99087019189
Shubhashish Verma <svashish305@gmail.com>
2020-08-05 15:15:51 +0530

    updated urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, re_path
 from rest_framework import routers
 from django.conf.urls import include
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    re_path('api/', include('peergroup.urls'))
 ]
