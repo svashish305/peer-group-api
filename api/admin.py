7fe4517382d447b8beb0baff02757f1976d9015e
Ekesh <ekeshtayal2804@gmail.com>
2020-08-06 19:24:19 +0530

    meeting model created

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Meeting
+admin.site.register(Meeting)
 
-# Register your models here.
--- api/models.py
+++ api/models.py
@@ -9,4 +9,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # class Feedback(models.Model):
 
-# class Meeting(models.Model):
+class Meeting(models.Model):
+    #groupid = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user=models.ManyToManyField(User)
+    meeting_Url=models.CharField(max_length=200,default=False,blank=False)
+    meeting_Time=models.DateTimeField(auto_now=False)
+
+    
