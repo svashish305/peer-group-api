2c33badaabceedc726264472cc7dd5ba13abc7cd
Shubhashish Verma <svashish305@gmail.com>
2020-08-20 12:37:41 +0530

    modified permissions

--- api/views.py
+++ api/views.py
@@ -20,7 +20,7 @@ class UserViewSet(viewsets.ModelViewSet):
         permission_classes = []
         if self.action == 'create':
             permission_classes = [AllowAny]
-        elif self.action == 'retrieve' or self.action == 'update':
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
             permission_classes = [IsLoggedInUserOrAdmin]
         elif self.action == 'list' or self.action == 'destroy':
             permission_classes = [IsAdminUser]
